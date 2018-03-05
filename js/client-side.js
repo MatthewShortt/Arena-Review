539ec257b0344365d94ac0c6a74f64f048fe15f3
Ryan Shortt <mattshortt4@gmail.com>
2018-03-04 21:37:37 -0500

    js additions

---
+++ js/client-side.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", function(event){
+    var x = document.getElementById("tester");
+
+    function test(){
+        x.innerHTML = "we made it";
+    }
+});
+
+
+
